fc68903fc1c0fad0f47e08ab9ab9e62cf2dbf270
Olesya <lesusha1@yandex.ru>
2023-11-02 13:10:46 +0300

    added two components

---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3>Name - {{ product.name }}</h3>
+    <h3>Price - {{ formattedPrice }}</h3>
+    <h3>
+      Availability -
+      <span v-if="product.availabe">Availlable</span>
+      <span v-else>Out of stock</span>
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: Object,
+  },
+  computed: {
+    formattedPrice() {
+      return `$${this.product.price}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  width: 300px;
+  border: 1px solid #000;
+  display: flex;
+  flex-direction: column;
+  padding-left: 20px;
+}
+</style>
---
+++ src/components/ProductDetailsComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <ProductComponent
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
+</template>
+
+<script>
+import ProductComponent from "./ProductComponent.vue";
+
+export default {
+  data() {
+    return {
+      products: [
+        {
+          name: "iphone15",
+          price: 199.99,
+          availabe: false,
+        },
+        {
+          name: "iphone13",
+          price: 99.99,
+          availabe: true,
+        },
+        {
+          name: "iphone8",
+          price: 39.99,
+          availabe: false,
+        },
+      ],
+    };
+  },
+  components: {
+    ProductComponent,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  display: flex;
+  gap: 20px;
+}
+</style>
